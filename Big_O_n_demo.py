e04df215c413333de87b7b510f52aaead7d06518
Rucha Khopkar <70127769+ruchakhopkar@users.noreply.github.com>
2024-08-22 15:38:36 +0530

    Update Big_O_n_demo.py

--- Big_O_n_demo.py
+++ Big_O_n_demo.py
@@ -1,3 +1,8 @@
+# Implement a Python function called print_items.
+# This function should take a single integer as an argument and print out a sequence of numbers from 0 up to, but not including, the provided integer.
+# The function should use a for loop and Python's built-in range function to generate the sequence of numbers.
+# The function signature should be: def print_items(n):
+
 ## WRITE THE PRINT_ITEMS FUNCTION HERE ##
 def print_items(n):
     for i in range(n):
